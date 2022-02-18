893a7d2def464d6aaabafcfa74260f5b19fd3528
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2022-02-18 21:55:14 +0200

    minor changes

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -2,7 +2,7 @@ function Preloader() {
   return (
     <div className="preloader" id="preloader">
         <div className="preloader-inner">
-          <img src="img/new/engineer.gif" alt="preloader"/>
+          <img src="img/new/Engineer.gif" alt="preloader"/>
         </div>
     </div>
   );
