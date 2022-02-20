a2c54853e0a9d70a011aabfb3dce0620b26220e8
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2022-02-20 20:24:34 +0200

    Minor updates

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,4 +1,4 @@
 export function getProjects() {
-    return  fetch('http://127.0.0.1:8000/api/projects/')
+    return  fetch('https://islandd-backend.herokuapp.com/api/projects/')
       .then(data => data.json())
   }
